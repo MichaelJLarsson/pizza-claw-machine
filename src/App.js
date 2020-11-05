b0bc9b68d075408f9221cb8340d8573827927753
Mikael Larsson <mikael.j.larsson@hotmail.com>
2020-11-05 21:32:09 +0100

    WIP: Static scene

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { ClawMachine } from "./claw-machine";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React Pizza claw machine</p>
-      </header>
-    </div>
-  );
+  return ClawMachine;
 }
 
 export default App;
---
+++ src/claw-machine.jsx
@@ -0,0 +1,11 @@
+export const ClawMachine = (
+  <div className="App">
+    <div className="claw">
+      <div className="claw-base"></div>
+      <div className="claw-arm-right"></div>
+      <div className="claw-arm-left">Claw left</div>
+    </div>
+    <div className="pizza-boxes"></div>
+    <div className="table-top"></div>
+  </div>
+);
