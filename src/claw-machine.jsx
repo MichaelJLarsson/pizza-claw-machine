f3b417874fc06fb3fbe5a1afa980d7ef945d0054
Mikael Larsson <mikael.j.larsson@hotmail.com>
2020-11-09 15:32:26 +0100

    Styled static scene

--- src/claw-machine.jsx
+++ src/claw-machine.jsx
@@ -1,11 +1,12 @@
 export const ClawMachine = (
   <div className="App">
     <div className="claw">
-      <div className="claw-base"></div>
-      <div className="claw-arm-right"></div>
-      <div className="claw-arm-left">Claw left</div>
+      <div className="claw-base" />
+      <div className="claw-arm claw-arm-right" />
+      <div className="claw-arm claw-arm-left" />
     </div>
-    <div className="pizza-boxes"></div>
-    <div className="table-top"></div>
+    <div className="pizza-box" />
+    <div className="pizza-boxes" />
+    <div className="table-top" />
   </div>
 );
